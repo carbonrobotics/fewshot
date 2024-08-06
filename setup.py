e3916cc181a3ff783ca40b29625be1484706f5a0
ZachNew <zachary@carbonrobotics.com>
2024-08-06 18:26:51 +0000

    added function to fetch version info from git

--- setup.py
+++ setup.py
@@ -1,9 +1,18 @@
+import subprocess
+
 from setuptools import setup
+
+def get_git_version():
+    try:
+        version = subprocess.check_output(["git", "describe", "--tags"]).strip().decode("utf-8")
+        return version
+    except subprocess.CalledProcessError:
+        return "0.0.0"  # Fallback version if the command fails
 
 if __name__ == "__main__":
     setup(
         name="fewshot",
-        version="1.0.0",
+        version=get_git_version(),
         description="The Python package for few-shot learning",
         author="Zachary New",
         author_email="",
